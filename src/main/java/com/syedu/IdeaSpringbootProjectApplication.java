1f91f85764f89f153521eec0958c59af149b0a0a
qudong <1766181826@qq.com>
2020-07-28 16:30:40 +0800

    Initial commit

---
+++ src/main/java/com/syedu/IdeaSpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.syedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaSpringbootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeaSpringbootProjectApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/syedu/controller/UserController.java
@@ -0,0 +1,5 @@
+package com.syedu.controller;
+
+public class UserController {
+
+}
